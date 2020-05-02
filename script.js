17a00a29c8ec4824700ce281ed9554ef8a3a4a17
sanboton <sanboton@users.noreply.github.com>
2020-05-02 04:10:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -212,3 +212,26 @@ var particles
 		particles.resize();
 	}
 }());
+
+
+///////////////// Twitter Hashtags //////////////////////////
+//////////////////////////////////////////////////////
+
+function search() {
+    TweetJs.Search("(#ES_F OR #CL_F OR #WTI OR #NQ_F OR #RTY_F OR #YM_F OR #stoxx OR #spx OR #spy OR #Futures OR #VIX OR #NDX OR #Hedgefunds OR #daytrading OR #$DJI OR #$DIA OR #DJI OR #DIA OR #stockmarket OR #swingtrading OR #technicalanalysis) lang:en",
+        function (data) {
+            console.log(data);
+            console.log(data.statuses[1]);
+            for (let i = 0; i <= data.statuses.length; i++) {
+                var dataString = '<div class="card" style="width: 18rem; margin-right: 1rem; margin-top: 1rem;">' + '<div class="card-body">'+
+                '<div class="row">'+
+                '<img src="' + data.statuses[i].user.profile_image_url+ '"'+
+                'class="card-img" alt="DP" style="width: 50px">'+
+                '<div class="column" id="userDetails">'+
+                '<h6>'+ data.statuses[i].user.name + '</h6><h6>@'+data.statuses[i].user.screen_name+'</h6></div></div>'+
+                '<div class="row">'+'<p>'+data.statuses[i].text+'</p>'+
+                '</div></div></div>'
+        document.getElementById("contentRow").innerHTML = document.getElementById("contentRow").innerHTML + dataString;
+            }
+        });
+}
